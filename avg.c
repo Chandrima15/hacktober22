2e918630298e6a8bdc5f4365d1c6f77bde51a78e
Anindita-2001 <116179362+Anindita-2001@users.noreply.github.com>
2022-10-21 12:04:37 +0530

    Update avg.c

--- avg.c
+++ avg.c
@@ -4,12 +4,12 @@ int main()
     int num1, num2;
     float avg;
 
-    printf("Enter first number: ");
+    printf("Enter first number: "); //Entering the first number
     scanf("%d",&num1);
-    printf("Enter second number: ");
+    printf("Enter second number: "); // Entering the second number
     scanf("%d",&num2);
 
-    avg= (float)(num1+num2)/2;
+    avg= (float)(num1+num2)/2; // Average of two numbers
 
     
     printf("Average of %d and %d is: %.2f",num1,num2,avg);
